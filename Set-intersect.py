f35bbc4ae5b4869fa71b9cdb42db65881eed40ba
Ayanacyan <amitpacl@gmail.com>
2026-03-12 22:17:55 -0500

    Intersection of sets

---
+++ Set-intersect.py
@@ -0,0 +1,9 @@
+setx={"green", "red", "yellow", "Blue", "purple"}
+sety={"White", "Black", "Pink", "purple"}
+
+print("Original set of elements")
+print(setx,"\n")
+print(sety,"\n")
+print("Intersection of 2 said sets")
+setz=setx.intersection(sety)
+print(setz)
